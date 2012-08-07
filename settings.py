04e29309e34ce0d95e552e935e67e865ce17fb92
Rob Terhaar <rob@atlanticdynamic.com>
2012-08-07 17:02:11 -0400

    update DATABASES config for multi-db in django >1.3

--- settings.py
+++ settings.py
@@ -10,12 +10,17 @@ ADMINS = (
 
 MANAGERS = ADMINS
 
-DATABASE_ENGINE = 'sqlite3'           # 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-DATABASE_NAME = 'dev.db'             # Or path to database file if using sqlite3.
-DATABASE_USER = ''             # Not used with sqlite3.
-DATABASE_PASSWORD = ''         # Not used with sqlite3.
-DATABASE_HOST = ''             # Set to empty string for localhost. Not used with sqlite3.
-DATABASE_PORT = ''             # Set to empty string for default. Not used with sqlite3.
+PROJECT_ROOT = os.path.dirname(__file__)
+DATABASES = {                                                                    
+    'default': {                                                                 
+        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': PROJECT_ROOT + '/dev.db', # Or path to database file if using sqlite3.
+        'USER': '',                      # Not used with sqlite3.                
+        'PASSWORD': '',                  # Not used with sqlite3.                
+        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
+        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
+    }                                                                            
+} 
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
